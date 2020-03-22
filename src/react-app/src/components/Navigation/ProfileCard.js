97946fddd89fe8d9d5b2d61eb7d8ef1fdcb9e0a2
grygielj <gryglos@gmail.com>
2020-03-22 15:31:39 +0100

    Added profileCard

---
+++ src/react-app/src/components/Navigation/ProfileCard.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Card from '@material-ui/core/Card';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Divider from "@material-ui/core/Divider";
+import Rating from '@material-ui/lab/Rating';
+import Button from '@material-ui/core/Button';
+import { shadows } from '@material-ui/system';
+
+
+
+
+const useStyles = makeStyles(()=>({
+    root: {
+        maxWidth: 360,
+        width:"30%",
+        minHeight:400,
+        borderRadius: 20,
+        display:"flex",
+        flexDirection: "column",
+        padding:10,
+        justifyContent:"space-around",
+        boxShadow: "0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)"
+    },
+    content:{
+
+    },
+    img:{
+        height:100,
+        width:100,
+        alignSelf:"center"
+    },
+    description:{
+        color:"gray"
+    },
+    rating:{
+      alignSelf:"center",
+    }
+
+}))
+
+
+const ProfileCard = ({image,name,description,rank}) => {
+    const cardStyles = useStyles();
+    return (
+        <>
+                <Card boxShadow={3} variant="outlined" className={cardStyles.root}>
+                    <Avatar className={cardStyles.img} alt={name} src={image} />
+                    <Typography variant="h4">
+                        {name}
+                    </Typography>
+                    <Divider/>
+                    <Typography variant="body1" className={cardStyles.description}>
+                        {description}
+                    </Typography>
+                    <Rating className={cardStyles.rating} name="ocena" value={rank} disabled/>
+                    <Button variant="contained" color="primary">
+                        Zobacz profil!
+                    </Button>
+                </Card>
+
+
+        </>
+    )
+};
+
+export default ProfileCard;
--- src/react-app/src/views/SearchView.js
+++ src/react-app/src/views/SearchView.js
@@ -1,5 +1,16 @@
 import React from "react";
+import ProfileCard from "../components/Navigation/ProfileCard";
 
 export default function SearchView() {
-    return <span>Search</span>;
+    return  (
+            <>
+                <span>Profile</span>
+                <ProfileCard image="https://learnreduxwithdanabramov.com/static/dan-abramov-photo.png"
+                             name="Dan Abramov"
+                             description="Making React accessible for users and developers at https://reach.tech . Online learning, workshops, OSS, and consulting."
+                             rank = "4"
+                > </ProfileCard>
+            </>
+        )
+
 }
