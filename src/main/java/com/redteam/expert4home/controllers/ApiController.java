d8a03a2ee3500247551eb289feb609deb1db64a9
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2020-04-04 17:57:44 +0200

    Api path prefix

--- src/main/java/com/redteam/expert4home/controllers/ApiController.java
+++ src/main/java/com/redteam/expert4home/controllers/ApiController.java
@@ -5,17 +5,19 @@ import com.redteam.expert4home.dao.entity.User;
 import lombok.var;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class ApiController {
 
     private Gson gson = new Gson();
 
-    @GetMapping("/api/expert")
+    @GetMapping("/expert")
     public ResponseEntity  getExpertsList() {
 
         List<User> experts = Arrays.asList(new User("John", "Travolta", "jt", "afaaf", false));
