5f7d5d6482f7d87f48707cf4b7ca03c1cdd7623e
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2020-04-06 18:40:50 +0200

    Bind code to app start

--- src/main/java/com/redteam/expert4home/dao/DBInitializer.java
+++ src/main/java/com/redteam/expert4home/dao/DBInitializer.java
@@ -1,4 +1,14 @@
 package com.redteam.expert4home.dao;
 
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
 public class DBInitializer {
+
+    @EventListener(ApplicationReadyEvent.class)
+    private void fillDatabaseWithInitialData() {
+        System.out.println("Now data shall be added");
+    }
 }
