77551774768f0033997b23cc1b66b1087b587c7a
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2020-04-09 17:57:55 +0200

    Switch to Repository that supports pagination

--- src/main/java/com/redteam/expert4home/dao/JobOrderRepository.java
+++ src/main/java/com/redteam/expert4home/dao/JobOrderRepository.java
@@ -1,7 +1,7 @@
 package com.redteam.expert4home.dao;
 
 import com.redteam.expert4home.dao.entity.JobOrder;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface JobOrderRepository extends CrudRepository<JobOrder, Long> {
+public interface JobOrderRepository extends PagingAndSortingRepository<JobOrder, Long> {
 }
--- src/main/java/com/redteam/expert4home/dao/UserRepository.java
+++ src/main/java/com/redteam/expert4home/dao/UserRepository.java
@@ -2,6 +2,7 @@ package com.redteam.expert4home.dao;
 
 import com.redteam.expert4home.dao.entity.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
 }
