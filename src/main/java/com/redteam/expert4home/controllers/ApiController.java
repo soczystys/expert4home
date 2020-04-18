fb122690128cefee7f32909510659df02ff1fcaf
Krzysztof Dąbrowski <01133318@pw.edu.pl>
2020-04-18 18:02:01 +0200

    Refactor to remove nested block

--- src/main/java/com/redteam/expert4home/controllers/ApiController.java
+++ src/main/java/com/redteam/expert4home/controllers/ApiController.java
@@ -20,6 +20,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
+
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
@@ -42,8 +43,7 @@ public class ApiController {
     }
 
     @GetMapping("/user/{id}")
-    public ResponseEntity<UserDTO> getSingeUser(@PathVariable Long id)
-    {
+    public ResponseEntity<UserDTO> getSingeUser(@PathVariable Long id) {
         Optional<User> user = userRepository.findById(id);
         if (!user.isPresent()) {
             return ResponseEntity.notFound().build();
@@ -57,38 +57,38 @@ public class ApiController {
             @RequestParam(name = "pageSize") Optional<Integer> pageSizeOptional,
             @RequestParam(name = "currentPageIndex") Optional<Integer> currentPageIndexOptional) {
 
-        if (pageSizeOptional.isPresent() && currentPageIndexOptional.isPresent()) {
-
-            int currentPageIndex = currentPageIndexOptional.get();
-            int pageSize = pageSizeOptional.get();
-            Pageable pageable = PageRequest.of(currentPageIndex, pageSize);
-            Page<User> experts = userRepository.findAll(pageable);
-
-//            TODO: create association to next page and previous
-            ExpertPage expertPage = new ExpertPage();
-            expertPage.setPageSize(pageSize);
-            expertPage.setCurrentPageIndex(currentPageIndex);
-            expertPage.setExperts(experts.get().map(dtoTranslator::createUserDTO).collect(Collectors.toList()));
-
-            if (experts.hasNext()) {
-                var linkToNex = linkTo(methodOn(ApiController.class).getExpertsList(pageSizeOptional, Optional.of(pageSize+1)))
-                        .withRel("nextPage");
-                expertPage.add(linkToNex);
-            }
-            if (experts.hasPrevious()) {
-                var linkToPrev = linkTo(methodOn(ApiController.class).getExpertsList(pageSizeOptional, Optional.of(pageSize-1)))
-                        .withRel("previousPage");
-                expertPage.add(linkToPrev);
-            }
-
-
-            return ResponseEntity.ok(expertPage);
-        } else {
+        if (!pageSizeOptional.isPresent() || !currentPageIndexOptional.isPresent()) {
             Iterable<User> iterator = userRepository.findAll();
-            List<User> experts =  StreamSupport
+            List<User> experts = StreamSupport
                     .stream(iterator.spliterator(), false)
                     .collect(Collectors.toList());
             return ResponseEntity.ok(gson.toJson(experts));
         }
+
+        int currentPageIndex = currentPageIndexOptional.get();
+        int pageSize = pageSizeOptional.get();
+        Pageable pageable = PageRequest.of(currentPageIndex, pageSize);
+        Page<User> experts = userRepository.findAll(pageable);
+
+//            TODO: create association to next page and previous
+        ExpertPage expertPage = new ExpertPage();
+        expertPage.setPageSize(pageSize);
+        expertPage.setCurrentPageIndex(currentPageIndex);
+        expertPage.setExperts(experts.get().map(dtoTranslator::createUserDTO).collect(Collectors.toList()));
+
+        if (experts.hasNext()) {
+            var linkToNex = linkTo(methodOn(ApiController.class).getExpertsList(pageSizeOptional, Optional.of(pageSize + 1)))
+                    .withRel("nextPage");
+            expertPage.add(linkToNex);
+        }
+        if (experts.hasPrevious()) {
+            var linkToPrev = linkTo(methodOn(ApiController.class).getExpertsList(pageSizeOptional, Optional.of(pageSize - 1)))
+                    .withRel("previousPage");
+            expertPage.add(linkToPrev);
+        }
+
+
+        return ResponseEntity.ok(expertPage);
+
     }
 }
