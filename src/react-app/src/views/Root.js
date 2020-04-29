a3f9b933ea997282f4aabcd433ad9fa1220f96f0
bartlomiej-zdrojewski <bartlomiej.zdrojewski@gmail.com>
2020-04-29 17:02:37 +0200

    Connected front-end and back-end for orders

--- src/react-app/src/views/Root.js
+++ src/react-app/src/views/Root.js
@@ -16,31 +16,64 @@ export const UserType = {
     EXPERT: 'EXPERT'
 };
 
-export default function Root() {
-    // TODO get user type from api
-    let userType = UserType.CLIENT;
+export class Root extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            userType: UserType.NONE
+        }
+    }
 
-    return (
-        <Router>
-            <Switch>
-                <Route path="/app/orders">
-                    <Navigation title={"Expert4Home | Zlecenia"} content={<OrdersView />} userType={userType} />
-                </Route>
-                <Route path="/app/profile">
-                    <Navigation title={"Expert4Home | Profil"} content={<ProfileView />} userType={userType} />
-                </Route>
-                <Route path="/app/search">
-                    <Navigation title={"Expert4Home | Specjaliści"} content={<SearchView />} userType={userType} />
-                </Route>
-                {/*
-                TODO remove/modify navigation
-                Added navigation for sample routing, but after making home screen remove it(in project home screen
-                not contain nav)!
-                */}
-                <Route path="/">
-                    <Navigation title={"Expert4Home | Specjaliści"} content={<HomeView />} userType={userType} />
-                </Route>
-            </Switch>
-        </Router>
-    );
+    componentDidMount() {
+        if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+            this.setState(prevState => {
+                return {
+                    ...prevState,
+                    userType: UserType.CLIENT
+                }
+            })
+        } else {
+            fetch('/api/users/current')
+                .then(res => { res.text().then(body => {
+                    fetch("/api/users/secure/" + body)
+                        .then(res => res.json())
+                        .then((data) => {
+                            this.setState(prevState => {
+                                return {
+                                    ...prevState,
+                                    userType: (data.expertMode === true ? UserType.EXPERT : UserType.CLIENT)
+                                }
+                            })
+                        })
+                    })
+                })
+                .catch(console.log)
+        }
+    }
+
+    render() {
+        return (
+            <Router>
+                <Switch>
+                    <Route path="/app/orders">
+                        <Navigation title={"Expert4Home | Zlecenia"} content={<OrdersView userType={this.state.userType} />} userType={this.state.userType} />
+                    </Route>
+                    <Route path="/app/profile">
+                        <Navigation title={"Expert4Home | Profil"} content={<ProfileView userType={this.state.userType} />} userType={this.state.userType} />
+                    </Route>
+                    <Route path="/app/search">
+                        <Navigation title={"Expert4Home | Specjaliści"} content={<SearchView userType={this.state.userType} />} userType={this.state.userType} />
+                    </Route>
+                    {/*
+                    TODO remove/modify navigation
+                    Added navigation for sample routing, but after making home screen remove it(in project home screen
+                    not contain nav)!
+                    */}
+                    <Route path="/">
+                        <Navigation title={"Expert4Home | Specjaliści"} content={<HomeView />} userType={this.state.userType} />
+                    </Route>
+                </Switch>
+            </Router>
+        );
+    }
 }
