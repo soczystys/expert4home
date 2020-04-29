11df39a5b0340aac7985de6d20de05da5d13ee14
soczystys <34406205+soczystys@users.noreply.github.com>
2020-04-29 10:26:34 +0200

    Add security template

--- src/main/java/com/redteam/expert4home/security/SecurityConfig.java
+++ src/main/java/com/redteam/expert4home/security/SecurityConfig.java
@@ -1,12 +1,20 @@
 package com.redteam.expert4home.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+import static com.redteam.expert4home.security.UserRole.*;
 
 @Configuration
 @EnableWebSecurity
@@ -22,18 +30,48 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()//TODO learn about that little nigga
                 .cors().and()
                 .authorizeRequests()
-                .antMatchers("/**").permitAll();
-//                .antMatchers("/css/*", "/js/*", "index", "/")
-//                .permitAll()
-//                .anyRequest()
-//                .authenticated()
-//                .and()
-//                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-//                .and()
-//                .formLogin();
-//                .loginPage("/login");
+                .antMatchers("/**")
+                .hasRole(ADMIN.name())
+//                .permitAll();
+                .anyRequest()
+                .authenticated()
+                .and()
+                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .and()
+                .formLogin()
+//                .loginPage("/login")
+//                .successForwardUrl()
+                .and()
+                .logout().deleteCookies("JSESSIONID")
+                .and()
+                .rememberMe()
+//                .tokenValiditySeconds((int) TimeUnit.SECONDS.toSeconds(20))
+                .key("deafvbtmetenmop");
+//                .rememberMeParameter("remember-me")
+//                .rememberMeCookieName("remember-me");
+//                .alwaysRemember(true);
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder.encode("admin"))
+                .roles(ADMIN.name())
+                .build();
+        UserDetails user = User.builder()
+                .username("user")
+                .password(passwordEncoder.encode("user"))
+                .roles(USER.name())
+                .build();
+        UserDetails expert = User.builder()
+                .username("expert")
+                .password(passwordEncoder.encode("expert"))
+                .roles(EXPERT.name())
+                .build();
+        return new InMemoryUserDetailsManager(admin, user, expert);
     }
 }
